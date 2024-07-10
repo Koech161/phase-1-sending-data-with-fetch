963022f4bbf13d8c525b8f52928baae0eb6c36ba
Abel Koech <koechabel161@gmail.com>
2024-07-10 20:31:49 +0300

    sending data

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData={
+    name:"Steve",
+    email:"steve@steve.com",
+}
+const configurationObject ={
+    method:"POST",
+    headers:{
+        "content-Type":"application/json",
+        "accept":"application/json",
+    },
+    body:JSON.stringify(submitData),
+}
+fetch("http://localhost:3000/users",configurationObject)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+
+})
+.catch(function(error){
+    alert('error detected')
+})
